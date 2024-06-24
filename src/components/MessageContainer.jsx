8c1a3e5c74c44f2dd8de9a614aaea583b3499395
Vishnu Gajulapalli <110832093+vishnu-blkbox@users.noreply.github.com>
2024-06-24 07:03:41 +0530

    use sockets for messaging

--- src/components/MessageContainer.jsx
+++ src/components/MessageContainer.jsx
@@ -1,18 +1,48 @@
 import { Avatar, Divider, Flex, Image, Skeleton, SkeletonCircle, Text, useColorModeValue } from "@chakra-ui/react"
 import Message from "./Message"
 import MessageInput from "./MessageInput"
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import showToast from "../hooks/showToast"
-import { useRecoilState, useRecoilValue } from "recoil"
-import { selectedConversationsAtom } from "../atoms/messagesAtom"
+import { useRecoilValue, useSetRecoilState } from "recoil"
+import { conversationsAtom, selectedConversationsAtom } from "../atoms/messagesAtom"
 import { userAtom } from "../atoms/userAtom"
+import { useSocket } from "../context/SocketContext"
 
 const MessageContainer = () => {
     const toast = showToast();
-    const [selectedConversation, setSelectedConversation] = useRecoilState(selectedConversationsAtom);
+    const selectedConversation = useRecoilValue(selectedConversationsAtom);
     const [loadingMessages, setLoadingMessages] = useState(true);
     const [messages, setMessages] = useState([]);
     const currentUser = useRecoilValue(userAtom);
+    const { socket } = useSocket();
+    const setConversations = useSetRecoilState(conversationsAtom);
+    const messageEndRef = useRef(null);
+
+    useEffect(() => {
+        socket.on('newMessage', (message) => {
+
+            if (selectedConversation._id === message.conversationId) {
+                setMessages((prevMessages) => [...prevMessages, message]);
+            }
+
+            setConversations((prev) => {
+				const updatedConversations = prev.map((conversation) => {
+					if (conversation._id === message.conversationId) {
+						return {
+							...conversation,
+							lastMessage: {
+								text: message.text,
+								sender: message.sender,
+							},
+						};
+					}
+					return conversation;
+				});
+				return updatedConversations;
+			});
+        });
+        return () => socket.off('newMessage');
+    }, [socket, selectedConversation, setConversations])
 
     useEffect(() => {
         const getMessages = async () => {
@@ -39,7 +69,11 @@ const MessageContainer = () => {
 
         getMessages();
 
-    }, [toast, selectedConversation])
+    }, [toast, selectedConversation]);
+
+    useEffect(() => {
+        messageEndRef.current?.scrollIntoView({ behavior: 'smooth' })
+    }, [messages])
 
     return (
         <Flex
@@ -89,7 +123,13 @@ const MessageContainer = () => {
 
                 {!loadingMessages && (
                     messages.map((message) => (
-                        <Message key={message._id} message={message} ownMessage={currentUser._id === message.sender} />
+                        <Flex
+                            key={message._id}
+                            direction={'column'}
+                            ref={messages.length -1 === messages.indexOf(message) ? messageEndRef : null}
+                        >
+                            <Message key={message._id} message={message} ownMessage={currentUser._id === message.sender} />
+                        </Flex>
                     ))
                 )}
             </Flex>
